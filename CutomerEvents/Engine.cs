617818a2f8ec8b32589ba8b94421eed3a2980813
Amr Salah Eldin <asalaheldinhassan@gmail.com>
2022-12-07 18:56:48 +0200

    Finalizing all the engine requirements

---
+++ CutomerEvents/Engine.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static TicketsConsole.Program;
+
+namespace CutomerEvents
+{
+
+    internal class Engine
+    {
+        private List<Event> Events { get; set; }
+        private Dictionary<(string, string), int> citiesDistances = new Dictionary<(string, string), int>();
+        public Engine(List<Event> events)
+        {
+            this.Events = events;
+        }
+        public void SendCustomerNotifications(Customer customer, Event e)
+        {
+            Console.WriteLine($"{customer.Name} from {customer.City} event {e.Name} at {e.Date}");
+        }
+        public void FilterEventsByCustomerCity(Customer customer, int count = 5)
+        {
+            var events = Events.Where(x => x.City == customer.City).Take(count);
+            foreach(var e in events)
+            {
+                SendCustomerNotifications(customer, e);
+            }
+        }
+        public void FilterEventsByCustomerBirthday(Customer customer, int count = 5)
+        {
+            var now = DateTime.UtcNow;
+            var birthdateDay = customer.BirthDate.Day;
+            var birthdateMonth = customer.BirthDate.Month;
+            var birthdateYear = customer.BirthDate.Year;
+
+            var events = this.Events.Where(x => x.Date > now
+            && (x.Date.Month >= birthdateMonth && x.Date.Day > birthdateDay) || (x.Date.Month > birthdateMonth))
+                .OrderBy(x => x.Date)
+                .Take(count)
+                .ToList();
+            foreach(var e in events)
+            {
+                SendCustomerNotifications(customer, e);
+            }
+        }
+        public void OrderEventsByClosest(Customer customer, int count = 5)
+        {
+            var events = Events
+                .OrderBy(x => GetDistances(customer.City, x.City))
+                .Take(count)
+                .ToList();
+            foreach(var e in events)
+            {
+                SendCustomerNotifications(customer, e);
+            }
+        }
+        public void SortEvents(Customer customer, Func<Event, int> func, int count = 5)
+        {
+            var events = Events
+                .OrderBy(func)
+                .Take(count)
+                .ToList();
+            foreach(var e in events)
+            {
+                SendCustomerNotifications(customer, e);
+            }
+        }
+        private int GetDistances(string cityA, string cityB, int tries = 5)
+        {
+            if (citiesDistances.ContainsKey((cityA, cityB))) return citiesDistances[(cityA, cityB)];
+            int distance = 0;
+            int counter = 0;
+            while(counter < tries)
+            {
+                try
+                {
+                    distance = AlphebiticalDistance(cityA, cityB);
+                    break;
+                }
+                catch(Exception e)
+                {
+                    counter++;
+                    if(counter >= tries)
+                    {
+                        Console.WriteLine($"Exception {e} happened with {cityA}, and {cityB}");
+                        throw;
+                    }
+                }
+            }
+            citiesDistances[(cityA, cityB)] = distance;
+            citiesDistances[(cityB, cityA)] = distance;
+            return distance;
+        }
+        private int AlphebiticalDistance(string s, string t)
+        {
+            var result = 0;
+            var i = 0;
+            for (i = 0; i < Math.Min(s.Length, t.Length); i++)
+            {
+                // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+                result += Math.Abs(s[i] - t[i]);
+            }
+            for (; i < Math.Max(s.Length, t.Length); i++)
+            {
+                // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+                result += s.Length > t.Length ? s[i] : t[i];
+            }
+
+            return result;
+        }
+    }
+}
--- CutomerEvents/Program.cs
+++ CutomerEvents/Program.cs
@@ -1,113 +1,121 @@
-﻿using System;
+﻿using CutomerEvents;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading.Tasks;
+using static TicketsConsole.Program;
 
-namespace CutomerEvents
+/*
+ 
+Let's say we're running a small entertainment business as a start-up. This means we're selling tickets to live events on a website. An email campaign service is what we are going to make here. We're building a marketing engine that will send notifications (emails, text messages) directly to the client and we'll add more features as we go.
+ 
+Please, instead of debuging with breakpoints, debug with "Console.Writeline();" for each task because the Interview will be in Coderpad and in that platform you cant do Breakpoints.
+ 
+*/
+
+namespace TicketsConsole
 {
-    class Event
+    internal class Program
     {
-        public string Name { get; set; }
-        public string City { get; set; }
-    }
-    class Customer
-    {
-        public string Name { get; set; }
-        public string City { get; set; }
-    }
-    class CityDistance
-    {
-        public int Distance { get; set; }
-        public string City { get; set; }
-    }
-
-    class Program
-    {
-        public static Dictionary<string, List<Event>> Events { get; set; }
-        static async Task Main(string[] args)
+        static HashSet<string> Cities = new HashSet<string>();
+        static Dictionary<string, EventWithDistance> CitiesDistances = new Dictionary<string, EventWithDistance>();
+        static void Main(string[] args)
         {
-            var events = new List<Event> {
-                new Event { Name = "Phantom of the Opera", City ="New York" },
-                new Event{ Name = "Metallica", City = "Los Angeles"},
-                new Event{ Name = "Metallica", City = "New York"},
-                new Event{ Name = "Metallica", City = "Boston"},
-                new Event{ Name = "LadyGaGa", City = "New York"},
-                new Event{ Name = "LadyGaGa", City = "Boston"},
-                new Event{ Name = "LadyGaGa", City = "Chicago"},
-                new Event{ Name = "LadyGaGa", City = "San Francisco"},
-                new Event{ Name = "LadyGaGa", City = "Washington"}
+            /*
+ 
+            1. You can see here a list of events, a customer object. Try to understand the code, make it compile. 
+
+ 
+            2.  The goal is to create a MarketingEngine class sending all events through the constructor as parameter and make it print the events that are happening in the same city as the customer. To do that, inside this class, create a SendCustomerNotifications method which will receive a customer as parameter and will mock the Notification Service. Add this ConsoleWriteLine inside the Method to mock the service. Inside this method you can add the code you need to run this task correctly but you cant modify the console writeline: Console.WriteLine($"{customer.Name} from {customer.City} event {e.Name} at {e.Date}");
+
+ 
+            3. As part of a new campaign, we need to be able to let customers know about events that are coming up close to their next birthday. You can make a guess and add it to the MarketingEngine class if you want to. So we still want to keep how things work now, which is that we email customers about events in their city or the event closest to next customer's birthday, and then we email them again at some point during the year. The current customer, his birthday is on may. So it's already in the past. So we want to find the next one, which is 23. How would you like the code to be built? We don't just want functionality; we want more than that. We want to know how you plan to make that work. Please code it.
+ 
+            4. The next requirement is to extend the solution to be able to send notifications for the five closest events to the customer. The interviewer here can paste a method to help you, or ask you to search it. We will attach 2 different ways to calculate the distance. 
+ 
+            // ATTENTION this row they don't tell you, you can google for it. In some cases, they pasted it so you can use it
+ 
+            Option 1:
+            var distance = Math.Abs(customerCityInfo.X - eventCityInfo.X) + Math.Abs(customerCityInfo.Y - eventCityInfo.Y);
+ 
+            Option 2:
+            private static int AlphebiticalDistance(string s, string t)
+            {
+                var result = 0;
+                var i = 0;
+                for(i = 0; i < Math.Min(s.Length, t.Length); i++)
+                    {
+                        // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+                        result += Math.Abs(s[i] - t[i]);
+                    }
+                    for(; i < Math.Max(s.Length, t.Length); i++)
+                    {
+                        // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+                        result += s.Length > t.Length ? s[i] : t[i];
+                    }
+                    
+                    return result;
+            } 
+ 
+            Tips of this Task:
+            Try to use Lamba Expressions. Data Structures. Dictionary, ContainsKey method.
+ 
+            5. If the calculation of the distances is an API call which could fail or is too expensive, how will you improve the code written in 4? Think in caching the data which could be code it as a dictionary. You need to store the distances between two cities. Example:
+ 
+            New York - Boston => 400 
+            Boston - Washington => 540
+            Boston - New York => Should not exist because "New York - Boston" is already stored and the distance is the same. 
+ 
+            6. If the calculation of the distances is an API call which could fail, what can be done to avoid the failure? Think in HTTPResponse Answers: Timeoute, 404, 403. How can you handle that exceptions? Code it.
+ 
+            7.  If we also want to sort the resulting events by other fields like price, etc. to determine whichones to send to the customer, how would you implement it? Code it.
+            */
+
+            
+
+            var events = new List<Event>{
+                new Event(1, "Phantom of the Opera", "New York", new DateTime(2023,12,23)),
+                new Event(2, "Metallica", "Los Angeles", new DateTime(2023,12,02)),
+                new Event(3, "Metallica", "New York", new DateTime(2023,12,06)),
+                new Event(4, "Metallica", "Boston", new DateTime(2023,10,23)),
+                new Event(5, "LadyGaGa", "New York", new DateTime(2023,09,20)),
+                new Event(6, "LadyGaGa", "Boston", new DateTime(2023,08,01)),
+                new Event(7, "LadyGaGa", "Chicago", new DateTime(2023,07,04)),
+                new Event(8, "LadyGaGa", "San Francisco", new DateTime(2023,07,07)),
+                new Event(9, "LadyGaGa", "Washington", new DateTime(2023,05,22)),
+                new Event(10, "Metallica", "Chicago", new DateTime(2023,01,01)),
+                new Event(11, "Phantom of the Opera", "San Francisco", new DateTime(2023,07,04)),
+                new Event(12, "Phantom of the Opera", "Chicago", new DateTime(2024,05,15))
             };
-            var customer = new Customer { Name = "Mr. Fake", City = "New York" };
+            var customer = new Customer()
+            {
+                Id = 1,
+                Name = "John",
+                City = "New York",
+                BirthDate = new DateTime(1995, 05, 10)
+            };
 
-            Events = new Dictionary<string, List<Event>>();
+            Console.WriteLine("How many results per filteration:");
+            Int32.TryParse(Console.ReadLine().ToString(), out int count);
 
-            //prepare list of events to be in dictionary
-            await PrepareEvents(events);
-
-            //Asking user which task it want to perform 
-            Console.WriteLine("Choose which task you need to perform 1 or 2:");
-            Int32.TryParse(Console.ReadLine(), out int choice);
+            var engine = new Engine(events);
+            Console.WriteLine($"1- Filter events by customer city: {customer.City}");
+            engine.FilterEventsByCustomerCity(customer, count);
             Console.WriteLine();
 
-            var closeEvents = choice == 2 ? await GetClosestEvents(customer.City) : Events[customer.City];
-            Console.WriteLine($"***Start Preparing email to customer: {customer.Name} from city: {customer.City}***");
+            Console.WriteLine($"2- Filter events by customer Birthdate: {customer.BirthDate}");
+            engine.FilterEventsByCustomerBirthday(customer, count);
             Console.WriteLine();
-            var tasks = new List<Task>();
-            foreach (var item in closeEvents.Take(5))
-            {
-                tasks.Add(AddToEmail(customer, item));
-            }
-            await Task.WhenAll(tasks);
+
+            Console.WriteLine($"3- Order events by closest to the customer in city: {customer.City}");
+            engine.OrderEventsByClosest(customer, count);
             Console.WriteLine();
-            Console.WriteLine($"***Finish Preparing email to customer: {customer.Name} from city: {customer.City}***");
-        }
 
-        
+            Console.WriteLine($"4- Sort events by Id");
+            engine.SortEvents(customer, x => x.Id, count);
+            Console.WriteLine();
 
-        static async Task PrepareEvents(List<Event> events)
-        {
-            foreach (var item in events)
-            {
-                if (!Events.ContainsKey(item.City))
-                    Events.Add(item.City, new List<Event>());
-
-                Events[item.City].Add(item);
-            }
         }
-        static async Task<List<Event>> GetClosestEvents(string customerCity)
-        {
-            var closeEvents = new List<Event>();
-            var orderedCityDistances = new SortedSet<CityDistance>(Comparer<CityDistance>.Create((a, b) => a.Distance.CompareTo(b.Distance)));
-            foreach(var item in Events)
-            {
-                orderedCityDistances.Add(new CityDistance() { Distance = await GetDistance(customerCity, item.Key), City = item.Key });
-            }
-            foreach(var cityDistance in orderedCityDistances)
-            {
-                closeEvents.AddRange(Events[cityDistance.City]);
-                if (closeEvents.Count >= 5)
-                    break;
-            }
-
-            return closeEvents;
-        }
-        static async Task AddToEmail(Customer c, Event e, int? price = null)
-        {
-            var distance = await GetDistance(c.City, e.City);
-            Console.Out.WriteLine($"{c.Name}: {e.Name} in {e.City}"
-            + (distance > 0 ? $" ({distance} miles away)" : "")
-            + (price.HasValue ? $" for ${price}" : ""));
-        }
-        static async Task<int> GetPrice(Event e)
-        {
-            return (await AlphebiticalDistance(e.City, "") + await AlphebiticalDistance(e.Name, "")) / 10;
-        }
-        static async Task<int> GetDistance(string fromCity, string toCity)
-        {
-            return await AlphebiticalDistance(fromCity, toCity);
-        }
-        static async Task<int> AlphebiticalDistance(string s, string t)
+        private static int AlphebiticalDistance(string s, string t)
         {
             var result = 0;
             var i = 0;
@@ -121,8 +129,198 @@ namespace CutomerEvents
                 // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
                 result += s.Length > t.Length ? s[i] : t[i];
             }
+
             return result;
         }
 
+        public class Event
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+            public DateTime Date { get; set; }
+
+            public Event(int id, string name, string city, DateTime date)
+            {
+                this.Id = id;
+                this.Name = name;
+                this.City = city;
+                this.Date = date;
+            }
+        }
+        public class EventWithDistance
+        {
+            public int Distance { get; set; }
+            public Event Event { get; set; }
+        }
+
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+            public DateTime BirthDate { get; set; }
+        }
+        public enum CampainMethod
+        {
+            City = 0,
+            Birthdate = 1,
+            Distance = 2
+        }
+
+
+        /*-------------------------------------
+        Coordinates are roughly to scale with miles in the USA
+           2000 +----------------------+  
+                |                      |  
+                |                      |  
+             Y  |                      |  
+                |                      |  
+                |                      |  
+                |                      |  
+                |                      |  
+             0  +----------------------+  
+                0          X          4000
+        ---------------------------------------*/
+
     }
 }
+
+
+#region old task commented
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics.CodeAnalysis;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace CutomerEvents
+//{
+
+
+//class Event
+//{
+//    public string Name { get; set; }
+//    public string City { get; set; }
+//}
+//class Customer
+//{
+//    public string Name { get; set; }
+//    public string City { get; set; }
+//}
+//class CityDistance
+//{
+//    public int Distance { get; set; }
+//    public string City { get; set; }
+//}
+
+//class Program
+//{
+
+
+
+
+//public static Dictionary<string, List<Event>> Events { get; set; }
+//static async Task Main(string[] args)
+//{
+//    var events = new List<Event> {
+//        new Event { Name = "Phantom of the Opera", City ="New York" },
+//        new Event{ Name = "Metallica", City = "Los Angeles"},
+//        new Event{ Name = "Metallica", City = "New York"},
+//        new Event{ Name = "Metallica", City = "Boston"},
+//        new Event{ Name = "LadyGaGa", City = "New York"},
+//        new Event{ Name = "LadyGaGa", City = "Boston"},
+//        new Event{ Name = "LadyGaGa", City = "Chicago"},
+//        new Event{ Name = "LadyGaGa", City = "San Francisco"},
+//        new Event{ Name = "LadyGaGa", City = "Washington"}
+//    };
+//    var customer = new Customer { Name = "Mr. Fake", City = "New York" };
+
+//    Events = new Dictionary<string, List<Event>>();
+
+//    //prepare list of events to be in dictionary
+//    await PrepareEvents(events);
+
+//    //Asking user which task it want to perform 
+//    Console.WriteLine("Choose which task you need to perform 1 or 2:");
+//    Int32.TryParse(Console.ReadLine(), out int choice);
+//    Console.WriteLine();
+
+//    var closeEvents = choice == 2 ? await GetClosestEvents(customer.City) : Events[customer.City];
+//    Console.WriteLine($"***Start Preparing email to customer: {customer.Name} from city: {customer.City}***");
+//    Console.WriteLine();
+//    var tasks = new List<Task>();
+//    foreach (var item in closeEvents.Take(5))
+//    {
+//        tasks.Add(AddToEmail(customer, item));
+//    }
+//    await Task.WhenAll(tasks);
+//    Console.WriteLine();
+//    Console.WriteLine($"***Finish Preparing email to customer: {customer.Name} from city: {customer.City}***");
+//}
+
+
+
+//static async Task PrepareEvents(List<Event> events)
+//{
+//    foreach (var item in events)
+//    {
+//        if (!Events.ContainsKey(item.City))
+//            Events.Add(item.City, new List<Event>());
+
+//        Events[item.City].Add(item);
+//    }
+//}
+//static async Task<List<Event>> GetClosestEvents(string customerCity)
+//{
+//    var closeEvents = new List<Event>();
+//    var orderedCityDistances = new SortedSet<CityDistance>(Comparer<CityDistance>.Create((a, b) => a.Distance.CompareTo(b.Distance)));
+//    foreach(var item in Events)
+//    {
+//        orderedCityDistances.Add(new CityDistance() { Distance = await GetDistance(customerCity, item.Key), City = item.Key });
+//    }
+//    foreach(var cityDistance in orderedCityDistances)
+//    {
+//        closeEvents.AddRange(Events[cityDistance.City]);
+//        if (closeEvents.Count >= 5)
+//            break;
+//    }
+
+//    return closeEvents;
+//}
+//static async Task AddToEmail(Customer c, Event e, int? price = null)
+//{
+//    var distance = await GetDistance(c.City, e.City);
+//    Console.Out.WriteLine($"{c.Name}: {e.Name} in {e.City}"
+//    + (distance > 0 ? $" ({distance} miles away)" : "")
+//    + (price.HasValue ? $" for ${price}" : ""));
+//}
+//static async Task<int> GetPrice(Event e)
+//{
+//    return (await AlphebiticalDistance(e.City, "") + await AlphebiticalDistance(e.Name, "")) / 10;
+//}
+//static async Task<int> GetDistance(string fromCity, string toCity)
+//{
+//    return await AlphebiticalDistance(fromCity, toCity);
+//}
+//static async Task<int> AlphebiticalDistance(string s, string t)
+//{
+//    var result = 0;
+//    var i = 0;
+//    for (i = 0; i < Math.Min(s.Length, t.Length); i++)
+//    {
+//        // Console.Out.WriteLine($"loop 1 i={i} {s.Length} {t.Length}");
+//        result += Math.Abs(s[i] - t[i]);
+//    }
+//    for (; i < Math.Max(s.Length, t.Length); i++)
+//    {
+//        // Console.Out.WriteLine($"loop 2 i={i} {s.Length} {t.Length}");
+//        result += s.Length > t.Length ? s[i] : t[i];
+//    }
+//    return result;
+//}
+
+//}
+
+//}
+#endregion
